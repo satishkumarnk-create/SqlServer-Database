62a98c3322cc0174d9374f690e8032657ccb0390
satishkumarnk <satishkumarnk@gmail.com>
2020-04-08 10:52:57 +0530

    Added Krishna

--- SqlServerDatabase/Product.sql
+++ SqlServerDatabase/Product.sql
@@ -3,5 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Name] VARCHAR(50) NULL, 
     [Cost] INT NULL,
-    [Color] VARCHAR(50) NULL
+    [Color] VARCHAR(50) NULL,
+    [Krishna] VARCHAR(100) null
 )
